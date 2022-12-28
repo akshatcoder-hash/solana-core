cbfb1389e404060ed089384982d08896d2f6ace1
akshatcoder-hash <akshatcoder@outlook.com>
2022-12-29 01:07:17 +0530

    Chewing Glass - 1 🍷

---
+++ core-1/Client-Side-Dev/solana-intro-frontend/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import AddressForm from '../components/AddressForm'
+import * as web3 from '@solana/web3.js'
+
+const Home: NextPage = () => {
+  const [balance, setBalance] = useState(0)
+  const [address, setAddress] = useState('')
+
+  const addressSubmittedHandler = (address: string) => {
+    const key = new web3.PublicKey(address);
+    setAddress(address)
+    setBalance(1000)
+  }
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p>
+          Start Your Solana Journey
+        </p>
+        <AddressForm handler={addressSubmittedHandler} />
+        <p>{`Address: ${address}`}</p>
+        <p>{`Balance: ${balance} SOL`}</p>
+      </header>
+    </div>
+  )
+}
+
+export default Home

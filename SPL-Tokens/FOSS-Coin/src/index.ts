d00bedb5546dc0b57faeb8a83a6f3fc2b5580551
Akshat Sharma <akshat@shastraos.co>
2023-01-15 19:04:40 +0530

    🪙 FOSSCU Coin

--- SPL-Tokens/FOSS-Coin/src/index.ts
+++ SPL-Tokens/FOSS-Coin/src/index.ts
@@ -1,8 +1,97 @@
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+} from "@metaplex-foundation/js"
+import {
+  DataV2,
+  createCreateMetadataAccountV2Instruction,
+  createUpdateMetadataAccountV2Instruction,
+} from "@metaplex-foundation/mpl-token-metadata"
+
+import * as fs from "fs"
+
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 
 // Add the spl-token import at the top
 import * as token from "@solana/spl-token"
+
+async function createTokenMetadata(
+  connection: web3.Connection,
+  metaplex: Metaplex,
+  mint: web3.PublicKey,
+  user: web3.Keypair,
+  name: string,
+  symbol: string,
+  description: string
+) {
+  // file to buffer
+  const buffer = fs.readFileSync("./assets/osi.png")
+
+  // buffer to metaplex file
+  const file = toMetaplexFile(buffer, "osi.png")
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+  // upload metadata and get metadata uri (off chain metadata)
+  const { uri } = await metaplex
+    .nfts()
+    .uploadMetadata({
+      name: name,
+      description: description,
+      image: imageUri,
+    })
+ 
+  console.log("metadata uri:", uri)
+
+  // get metadata account address
+  const metadataPDA = metaplex.nfts().pdas().metadata({mint})
+
+  // onchain metadata format
+  const tokenMetadata = {
+    name: name,
+    symbol: symbol,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  } as DataV2
+
+  // transaction to create metadata account
+  const transaction = new web3.Transaction().add(
+    createCreateMetadataAccountV2Instruction(
+      {
+        metadata: metadataPDA,
+        mint: mint,
+        mintAuthority: user.publicKey,
+        payer: user.publicKey,
+        updateAuthority: user.publicKey,
+      },
+      {
+        createMetadataAccountArgsV2: {
+          data: tokenMetadata,
+          isMutable: true,
+        },
+      }
+    )
+  )
+
+  // send transaction
+  const transactionSignature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [user]
+  )
+
+  console.log(
+    `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
 
 async function createNewMint(
     connection: web3.Connection,
@@ -123,52 +212,37 @@ async function mintTokens(
     )
   }
 
-  async function main() {
-    const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-    const user = await initializeKeypair(connection)
-  
-    console.log("PublicKey:", user.publicKey.toBase58())
-  
-    const mint = await createNewMint(
-      connection,
-      user,           // We'll pay the fees
-      user.publicKey, // We're the mint authority
-      user.publicKey, // And the freeze authority >:)
-      2               // Only two decimals!
-    )
-  
-    const tokenAccount = await createTokenAccount(
-      connection,     
-      user,           
-      mint,            
-      user.publicKey   // Associating our address with the token account
-    )
-    
-    const receiver = web3.Keypair.generate().publicKey
-    
-    const receiverTokenAccount = await createTokenAccount(
-        connection,
-        user,
-        mint,
-        receiver
-    )
+async function main() {
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
+  const user = await initializeKeypair(connection)
 
-    await transferTokens(
-        connection,
-        user,
-        tokenAccount.address,
-        receiverTokenAccount.address,
-        user.publicKey,
-        50,
-        mint
-    )
-    
-   await burnTokens(connection, user, tokenAccount.address, mint, user, 25)
+  console.log("PublicKey:", user.publicKey.toBase58())
 
-    // Mint 100 tokens to our address
-    await mintTokens(connection, user, mint, tokenAccount.address, user, 100)
-  }
+  const MINT_ADDRESS = "4PeYAUaM2eTE9nm4vq8agZ2nhzTdPxMJZ4L2cAgcbhGj"
+
+  // metaplex setup
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    )
   
+  // Calling the token 
+  await createTokenMetadata(
+    connection,
+    metaplex,
+    new web3.PublicKey(MINT_ADDRESS),
+    user,
+    "FOSS Coin", // Token name
+    "FOSSCU",     // Token symbol
+    "Whoever holds this token is invited FOSS Community" // Token description
+  )
+}
+
 main()
   .then(() => {
     console.log("Finished successfully")

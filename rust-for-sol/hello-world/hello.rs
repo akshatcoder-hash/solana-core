c3269d60e54c641c0e440275c7f7107b97a00302
Akshat Sharma <akshatcoder@outlook.com>
2023-05-18 09:32:46 +0530

    🦀 Macros and Functions

--- rust-for-sol/hello-world/hello.rs
+++ rust-for-sol/hello-world/hello.rs
@@ -5,3 +5,15 @@ use solana_program::{
     pubkey::Pubkey,
     msg
 };
+
+entrypoint!(process_instruction)
+
+pub fn process_instruction(
+    //Arguments and their types
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+    // The return type (i.e. what data type the function returns)
+) -> ProgramResult{
+  // Leave the body empty for now :)
+}
